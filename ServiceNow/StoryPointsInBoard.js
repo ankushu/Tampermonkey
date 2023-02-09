ec1838e0d9345382ebd248beeceeb4b36a7de0ec
Ankush Agrawal <Ankush.Agrawal@servicenow.com>
2023-02-09 10:14:57 +0530

    Story points in board for both SAFe and Agile

--- ServiceNow/StoryPointsInBoard.js
+++ ServiceNow/StoryPointsInBoard.js
@@ -15,9 +15,14 @@
     var iter = 0;
     setTimeout(setPoints, 1000);
 
+    //Announcement
+    /*let title = $('label.sn-navhub-title').text();
+    $('label.sn-navhub-title').text(title + ' ***\t' + 'Confirmation for the year end party');*/
+
     function setPoints() {
         var storyNumberSelector = '.open-record-button';
         var nodeStories = $(storyNumberSelector);
+        var nodeUATStories = $('li:contains("In UAT")[class*="vtb-lane"] ' + storyNumberSelector);
 
         if (nodeStories.length === 0) {
             if(++iter === 30) {
@@ -33,7 +38,8 @@
             storyNumbers.push(nodeStories[i].text);
 
         // storyNumbers.forEach((e, i) => {
-            $.get('/api/now/table/sn_safe_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,sn_safe_feature.sys_id,sn_safe_feature.short_description', function (data) {
+        // $.get('/api/now/table/sn_safe_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,sn_safe_feature.sys_id,sn_safe_feature.short_description,priority', function (data) { //SAFe
+            $.get('/api/now/table/rm_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,epic.sys_id,epic.short_description,priority', function (data) { //Agile
                 data.result.forEach(e => {
                     var nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e.number + '")');
                     nodeSTRYNumber.text(e.number + ' (' + e.story_points + ')');
@@ -41,18 +47,29 @@
                         markDefect(_getCardID(nodeSTRYNumber));
                         // nodeSTRYNumber.css('color', 'red');
 
-                    var safeFeature = e['sn_safe_feature.short_description'];
+                    var safeFeature = e['epic.short_description']; //Feature e['sn_safe_feature.short_description'];
                     if (!safeFeature)
                         return;
 
+                    let featureColor = stringToColour(safeFeature);
                     //var nodeStryDescr = nodeSTRYNumber.parents('.vtb-card-component-wrapper').children('.sn-card-component_summary');
                     //var descr = nodeStryDescr.text();
                     //nodeStryDescr.html('<span style="color:brown;">' + getEpicPrintableText(sn_safe_feature) + '</span><br/>'+ descr);
-                   //nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<span style="color:#222;"><a href="/rm_epic.do?sys_id=' + e['epic.sys_id'] + '">' + getEpicPrintableText(epic) + '</a></span>');
-                    nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
+                    nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/rm_epic.do?sys_id=' + e['epic.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '(' + e['priority'] + ')' + '</a>');
+                    // nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '(' + e['priority'] + ')' + '</a>');
                 });
+                addRefreshButton(this);
             });
         // });
+
+        function addRefreshButton(that) {
+            if ($('#setPoints').length !== 0)
+                return;
+            
+            top.setPoints = setPoints;
+            let existingHTML = $('.sn-navhub-content_row').children('div').html();
+            $('.sn-navhub-content_row').children('div').html(existingHTML + '<button id="setPoints" onclick="top.setPoints()">Refresh Additional Info</button>');
+        }
 
         function getEpicPrintableText(epicShortDescrt) {
             return epicShortDescrt.trim();
@@ -72,5 +89,35 @@
                 dataType: 'application/json'
             });
         }
+
+        var stringToColour = function(str) {
+            var hash = 0;
+            for (var i = 0; i < str.length; i++) {
+                hash = str.charCodeAt(i) + ((hash << 5) - hash);
+            }
+            var colour = '#';
+            for (var j = 0; j < 3; j++) {
+                var value = (hash >> (j * 8)) & 0xFF;
+                colour += ('00' + value.toString(16)).slice(-2);
+            }
+            return colour;
+        }
+
+        const getPOATask = function () {
+            debugger;
+            if (nodeUATStories.length === 0)
+                return;
+            let uatStoriesNumbers = [];
+            for (var i = 0; i < nodeStories.length; i++)
+                uatStoriesNumbers.push(nodeUATStories[i].text);
+            $.get('/api/now/table/sn_safe_scrum_task?sysparm_query=sn_safe_story.numberIN' + uatStoriesNumbers.join(',') + '^type=7 - UAT&sysparm_fields=number,sys_id', function (data) {
+                data.result.forEach(e => {
+                    debugger;
+                    let nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e.number + '")');
+                    nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
+                });
+                
+            });
+        } 
     }
 })();
