5224d8e7606d7bfb9e26b4ebacf4c87fd82debd6
Ankush Agrawal <Ankush.Agrawal@servicenow.com>
2023-02-27 19:22:16 +0530

    Special handling for directs goals

--- ServiceNow/Manage goals.js
+++ ServiceNow/Manage goals.js
@@ -5,6 +5,7 @@
 // @description  Better manage goals in Surf
 // @author       ankush.agrawal
 // @match        https://surf.service-now.com/*pd_my_goals*
+// @match        https://surf.service-now.com/*pd_manage_goals*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -15,7 +16,7 @@
   setTimeout(manageGoals, 1000);
 
   function manageGoals() {
-    if ($(".goal-actions").length === 0) {
+    if ($(".goal-container").length === 0) {
       if (++iter === 30) {
         console.error("Could not find Stories in 30 iterations");
         return;
@@ -25,16 +26,19 @@
       return;
     }
       
-    function createOpenGoalNode(goal) {
+      function createOpenGoalNode(goal) {
+          let reviewGoalsQuery = '';
+          if (window.getSpPageUrl().indexOf('manage_goals') !== -1)
+            reviewGoalsQuery = '&goal_action=review'
       const nodeSpan = document.createElement("span");
       nodeSpan.setAttribute("class", "glyphicon glyphicon-new-window");
       const nodeAnchor = document.createElement("a");
-      nodeAnchor.setAttribute("href", "/esc?id=pd_goals_form&gid=" + goal.gid);
+      nodeAnchor.setAttribute("href", "/esc?id=pd_goals_form&gid=" + goal.gid + reviewGoalsQuery);
       nodeAnchor.setAttribute("target", "_blank");
       nodeAnchor.appendChild(nodeSpan);
       return nodeAnchor;
     }
-    $(".goal-actions").each(function (goalAction) {
+    $(".goal-container").each(function (goalAction) {
       this.appendChild(createOpenGoalNode(angular.element(this).scope().goal));
     });
   }
