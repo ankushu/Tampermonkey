d450bef749246f42e06f619695d0954dad31abfe
Ankush Agrawal <Ankush.Agrawal@servicenow.com>
2023-04-10 22:58:20 +0530

    Count for story assigned to each dev

--- ServiceNow/StoryPointsInBoard.js
+++ ServiceNow/StoryPointsInBoard.js
@@ -12,7 +12,8 @@
 
 (function () {
     'use strict';
-    var iter = 0;
+    let iter = 0;
+    let sprint = {};
     setTimeout(setPoints, 1000);
 
     //Announcement
@@ -25,7 +26,7 @@
         var nodeUATStories = $('li:contains("In UAT")[class*="vtb-lane"] ' + storyNumberSelector);
 
         if (nodeStories.length === 0) {
-            if(++iter === 30) {
+            if (++iter === 30) {
                 console.error('Could not find Stories in 30 iterations');
                 return;
             }
@@ -36,36 +37,38 @@
         var storyNumbers = [];
         for (var i = 0; i < nodeStories.length; i++)
             storyNumbers.push(nodeStories[i].text);
+        
+        getSprintMetadata();
 
         // storyNumbers.forEach((e, i) => {
         // $.get('/api/now/table/sn_safe_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,sn_safe_feature.sys_id,sn_safe_feature.short_description,priority', function (data) { //SAFe
-            $.get('/api/now/table/rm_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,epic.sys_id,epic.short_description,priority', function (data) { //Agile
-                data.result.forEach(e => {
-                    var nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e.number + '")');
-                    nodeSTRYNumber.text(e.number + ' (P' + e.priority + '/' + e.story_points + 'Pt)');
-                    if (e.classification === 'Defect')
-                        markDefect(_getCardID(nodeSTRYNumber));
-                        // nodeSTRYNumber.css('color', 'red');
+        $.get('/api/now/table/rm_story?sysparm_query=numberIN' + storyNumbers.join(',') + '&sysparm_fields=number,story_points,classification,epic.sys_id,epic.short_description,priority', function (data) { //Agile
+            data.result.forEach(e => {
+                var nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e.number + '")');
+                nodeSTRYNumber.text(e.number + ' (P' + e.priority + '/' + e.story_points + 'Pt)');
+                if (e.classification === 'Defect')
+                    markDefect(_getCardID(nodeSTRYNumber));
+                // nodeSTRYNumber.css('color', 'red');
 
-                    var safeFeature = e['epic.short_description']; //Feature e['sn_safe_feature.short_description'];
-                    if (!safeFeature)
-                        return;
+                var safeFeature = e['epic.short_description']; //Feature e['sn_safe_feature.short_description'];
+                if (!safeFeature)
+                    return;
 
-                    let featureColor = stringToColour(safeFeature);
-                    //var nodeStryDescr = nodeSTRYNumber.parents('.vtb-card-component-wrapper').children('.sn-card-component_summary');
-                    //var descr = nodeStryDescr.text();
-                    //nodeStryDescr.html('<span style="color:brown;">' + getEpicPrintableText(sn_safe_feature) + '</span><br/>'+ descr);
-                    nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/rm_epic.do?sys_id=' + e['epic.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
-                    // nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
-                });
-                addRefreshButton(this);
+                let featureColor = stringToColour(safeFeature);
+                //var nodeStryDescr = nodeSTRYNumber.parents('.vtb-card-component-wrapper').children('.sn-card-component_summary');
+                //var descr = nodeStryDescr.text();
+                //nodeStryDescr.html('<span style="color:brown;">' + getEpicPrintableText(sn_safe_feature) + '</span><br/>'+ descr);
+                nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/rm_epic.do?sys_id=' + e['epic.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
+                // nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
             });
+            addRefreshButton(this);
+        });
         // });
 
         function addRefreshButton(that) {
             if ($('#setPoints').length !== 0)
                 return;
-            
+
             top.setPoints = setPoints;
             let existingHTML = $('.sn-navhub-content_row').children('div').html();
             $('.sn-navhub-content_row').children('div').html(existingHTML + '<button id="setPoints" onclick="top.setPoints()">Refresh Additional Info</button>');
@@ -90,7 +93,7 @@
             });
         }
 
-        var stringToColour = function(str) {
+        var stringToColour = function (str) {
             var hash = 0;
             for (var i = 0; i < str.length; i++) {
                 hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -103,21 +106,55 @@
             return colour;
         }
 
-        const getPOATask = function () {
+        function getSprintMetadata() {
+            $.get('/api/now/table/rm_story?sysparm_query=number=' + storyNumbers[0] + '&sysparm_fields=sprint,sprint.short_description', function (data) { //Agile
+                ({
+                    sprint: {
+                        value: sprint.sys_id
+                    },
+                    'sprint.short_description': sprint.short_description
+                } = data.result[0]);
+                getStoryCountPerDev();
+            });
+        }
+
+        function getStoryCountPerDev() {
             debugger;
+            $.get("/api/now/stats/rm_story?sysparm_query=sprint=" + sprint.sys_id + "^stateIN-6,1,-5,-7,-4,-2^assigned_toISNOTEMPTY&sysparm_count=true&sysparm_group_by=assigned_to.name", function (data) {
+                const storyPerDev = data.result.map(e => {
+                    const USER = e.groupby_fields[0].value,
+                        COUNT = e.stats.count
+                    $('.vtb-filter-pill-label:contains("' + USER + '")').text(USER + ' (' + COUNT + ')');
+                });
+            });
+        }
+
+        const getPOATask = function () {
+            /**
+             * New design
+             * 0. Add the button on STRY to close the task and move the story both
+             * 1. Add the buttons to story title initially itself when we query the stories. 
+             *   1.a we can also get the state of the story and run this only for the stories in 'In UAT'
+             * 2. The button only acceptes the story number and has the function (acceptStory) --> untested done
+             * 3. Fetch POA task only when the butotn is clicked, assign to self and close, move the story to accept
+             * 
+             */
+            const IMG_MOVE_RIGHT = '/images/toggle_right.gif';
             if (nodeUATStories.length === 0)
                 return;
             let uatStoriesNumbers = [];
-            for (var i = 0; i < nodeStories.length; i++)
-                uatStoriesNumbers.push(nodeUATStories[i].text);
-            $.get('/api/now/table/sn_safe_scrum_task?sysparm_query=sn_safe_story.numberIN' + uatStoriesNumbers.join(',') + '^type=7 - UAT&sysparm_fields=number,sys_id', function (data) {
+            for (var i = 0; i < nodeUATStories.length; i++)
+                uatStoriesNumbers.push(nodeUATStories[i].text.split(' ')[0]);
+            $.get('/api/now/table/rm_scrum_task?sysparm_query=sn_safe_story.numberIN' + uatStoriesNumbers.join(',') + '^ORrm_story.numberIN' + uatStoriesNumbers.join(',') + '^type=7 - UAT&sysparm_fields=number,sys_id,rm_story.number', function (data) {
                 data.result.forEach(e => {
                     debugger;
-                    let nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e.number + '")');
-                    nodeSTRYNumber.parents('.vtb-card-component-meta-wrapper').children('.vtb-compact-sla-checklist').html('<a class="sn-card-component-detail open-record-button ng-binding" style="color:' + featureColor + '" role="button" href="/sn_safe_feature.do?sys_id=' + e['sn_safe_feature.sys_id'] + '">' + getEpicPrintableText(safeFeature) + '</a>');
-                });
-                
-            });
-        } 
+                    //TODO
+                    let nodeSTRYNumber = $(storyNumberSelector + ':contains("' + e['rm_story.number'] + '")');
+                    let nodeStryTitle = nodeSTRYNumber.parents('.vtb-card-component-wrapper').find('.vtb-card-header-default');
+                    nodeStryTitle.html(nodeStryTitle.text() + '<button onclick="acceptStory(' + e + ')"><img src="/images/toggle_right.gif"></img></button>');
+                }, this);
+
+            }, this);
+        }
     }
 })();
